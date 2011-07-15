c75782406b1079a47c89a1c27b44310e05f8b718
Enund The Scratch <ivvlg@narod.ru>
2011-07-15 21:48:40 +0400

    Working with default MembershipProvider in ASP.NET

---
+++ Web_01/Web_01/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_01
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                MembershipProvider CurrentProvider = Membership.Provider;
+                lblName.Text = "Current Membership Provider is " + CurrentProvider.Name;
+                MembershipUser CurrentUser = Membership.CreateUser("abc", "abcabc_");
+                lblName.Text += " and currently created user name is " + CurrentUser.UserName;
+                Membership.DeleteUser("abc");
+            }
+        }
+    }
+}
